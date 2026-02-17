1207e5c010e2fabe2180b7832e26caa5c6c8c8ea
Ankur Seth <iankurseth@gmail.com>
2026-02-17 22:06:48 +0530

    Ignoring two-tone icons for now

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 
 ICONS_DIRPATH = "material-icons/svg"
+BASE_DIR = Path(ICONS_DIRPATH).resolve()
 
 # precompile for speed (handles single or double quotes, any attr order)
 _PATH_D_RE = re.compile(
@@ -20,9 +21,16 @@ def extract_svg_pathd(svg_file: str) -> str | None:
 
 
 class MaterialIcon:
-	def __init__(self, name, fullpath):
-		self.name = name
+	def __init__(self, fullpath):
 		self.fullpath = fullpath
+
+		relative_path = self.fullpath.relative_to(BASE_DIR)
+		fullname = relative_path.with_suffix("").as_posix()
+		self.category, self.name = fullname.replace("_", "-").split("/")
+
+	@property
+	def fullname(self):
+		return f"{self.category}-{self.name}"
 
 	@property
 	def pathd(self):
@@ -30,30 +38,20 @@ class MaterialIcon:
 
 	@property
 	def jo(self):
-		return dict(name=self.name, pathd=self.pathd)
+		return dict(name=self.fullname, pathd=self.pathd)
 
 	def __lt__(self, other):
 		return self.fullpath < other.fullpath
 
 	def __str__(self):
-		return f"{self.name} ({self.fullpath})"
+		return f"{self.fullname} ({self.fullpath})"
 
 
 def find_svgs():
-	base = Path(ICONS_DIRPATH).resolve()
 	icons = []
 
-	for svg_path in base.rglob("*.svg"):
-		rel_path = svg_path.relative_to(base)
-
-		# normalize to forward slashes for consistency
-		rel_posix = rel_path.as_posix()
-
-		# build name: relative path (no extension), / and _ -> -
-		name = rel_path.with_suffix("").as_posix()
-		name = name.replace("/", "-").replace("_", "-")
-
-		icon = MaterialIcon(name, svg_path)
+	for svg_path in BASE_DIR.rglob("*.svg"):
+		icon = MaterialIcon(svg_path)
 		icons.append(icon)
 
 	icons.sort()
@@ -67,8 +65,9 @@ def print_icons(icons):
 
 def main():
 	icons = find_svgs()
+	icons = [icon for icon in icons if icon.category != 'two-tone']
 	print(f"Found {len(icons)} icons")
-	# print_icons(icons[:10])
+	print_icons(icons[:10])
 
 	data = [icon.jo for icon in icons]
 	jo = dict(icons=data)
